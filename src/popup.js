8d7364775ec75a27ec5d75fa8b764f6d0e251969
flaxinger <70012548+flaxinger@users.noreply.github.com>
2021-09-25 00:03:57 +0900

    09-25--concat-implemented

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,10 @@
+
+// Query Object
+// mainQuery = Query end users want to search
+// searchSite = Sites end users want to see
+// removeSites = Sites end users don't want to see
+// fileType = Filetype end users want to find
+// result = concatnated query result
 function queryRefine(mainQuery="", searchSite="", removeSites="", fileType="", result=""){
     this.mainQuery = mainQuery;
     this.searchSite = searchSite;
@@ -7,28 +14,39 @@ function queryRefine(mainQuery="", searchSite="", removeSites="", fileType="", r
 }
 
 queryRefine.prototype.refine = function (){
-    this.result = this.mainQuery;
+    this.result = "https://google.com/search?q=";
+    if(this.mainQuery.length > 0) 
+        this.result += this.mainQuery.trim().replaceAll(/\s+/ig, "+");
     if(this.removeSites.length > 0){
-        this.result += " -site:" + this.removeSites; 
+        this.result += "+"
+        this.result += this.removeSites.trim().replaceAll(/\s+/ig, "-site:");
     }
     if(this.fileType.length > 0){
-        this.result += " filetype:" + this.fileType;
+        this.result+="+filetype:";
+        this.result += this.fileType.trim().replaceAll(/\s+/ig, "+OR+filetype:");
     }
+    if(this.searchSite.length > 0){
+        this.result+="&as_sitesearch=";
+        this.result += this.searchSite.trim().replaceAll(/\s+/ig, "+");
+    }
+
     return this.result;
 }
-
-//let rawQuery = queryRefine;
 
 document.getElementById('proceedSearch').onclick = () => {
     let rawQuery = new queryRefine(document.getElementById("searchString").value, 
                             document.getElementById("searchSites").value,
                             document.getElementById("removeSites").value, 
                             document.getElementById("fileType").value);
-    //console.log(rawQuery.refine());
-    chrome.tabs.executeScript({
-        //code: 'var userQuery = '+ JSON.stringify(rawQuery.result)
-        code: 'var userQuery = '+ JSON.stringify(rawQuery.refine())
-    }, function () {
-        chrome.tabs.executeScript({file: 'src/content.js'});
-    });
+
+
+    // chrome.tabs.create(url=rawQuery.refine());
+    window.open(url=rawQuery.refine());
+    window.location.replace(url=rawQuery.refine());
+    // chrome.tabs.executeScript({
+    //     //code: 'var userQuery = '+ JSON.stringify(rawQuery.result)
+    //     code: 'var userQuery = '+ JSON.stringify(rawQuery.refine())
+    // }, function () {
+    //     chrome.tabs.executeScript({file: 'src/content.js'});
+    // });
 };
